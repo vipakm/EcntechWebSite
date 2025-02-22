db31cfb2b9e140de2b08e8853bc70a5eaa2237c9
Adbspec <abhishekr0709@gmail.com>
2025-02-23 02:16:09 +0530

    added home, services, products and about section

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ecnt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
